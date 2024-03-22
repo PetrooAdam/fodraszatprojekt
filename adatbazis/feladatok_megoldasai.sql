4943cbc45e8f2c04514076faef07a173fdb0fa6a
Bajor Szabolcs <bajor.szabolcs@ckik.loc>
2024-03-22 11:30:27 +0100

    1 kozepes, 3 nehez, 3 megoldas

--- adatbazis/feladatok_megoldasai.sql
+++ adatbazis/feladatok_megoldasai.sql
@@ -1,5 +1,8 @@
 use fodraszat_adatbazis;
 
+
+-- Konnyu feladatok
+
 -- 1. feladat
 select * 
 from fodrasz;
@@ -21,5 +24,39 @@ where email LIKE "%@ckik.hu";
 
 -- 5. feladat
 UPDATE fodrasz
-set email = "lonelybarber@outlook.com "
+set email = "lonelybarber@outlook.com"
 where nev = "Sándor Marcell Szabolcs";
+
+
+
+
+-- Kozepes feladatok
+
+-- 1. feladat
+select nev
+from fodrasz
+where telefonszam like "+3620%"
+union
+select nev
+from fodrasz
+where telefonszam like "0620%";
+
+
+
+
+
+-- Nehez feladatok 
+
+
+-- 1. feladat
+select szolgaltatas_nev, vendeg_telefonszam, uzlet_id, telefonszam, nev, email
+from foglalas f inner join fodrasz t on f.fodrasz_telefonszam = t.telefonszam 
+where fodrasz_telefonszam = "06701923124";
+
+
+-- 2. feladat
+select distinct t.nev
+from foglalas f inner join fodrasz t on f.fodrasz_telefonszam = t.telefonszam inner join vendeg v on f.vendeg_telefonszam = v.telefonszam
+where ROUND((LENGTH(v.nev) - LENGTH( REPLACE (v.nev, " ", "") )) / LENGTH(" ")) = 2
+order by t.nev ASC;
+
